81ec975328ba338d5b0feeaa8ef7cff082c5bbfd
mpbrown <mpbrown@buffalo.edu>
2020-03-25 02:20:28 -0400

    Add buffalomutualaid.org to allowed hosts

--- buffalomutualaid/settings/production.py
+++ buffalomutualaid/settings/production.py
@@ -17,7 +17,7 @@ DATABASES['default'] =  dj_database_url.config()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
-ALLOWED_HOSTS = ['localhost','buffalomutualaid.herokuapp.com']
+ALLOWED_HOSTS = ['localhost','buffalomutualaid.herokuapp.com','buffalomutualaid.org']
 
 
 try:
