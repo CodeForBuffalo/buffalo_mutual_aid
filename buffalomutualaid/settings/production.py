1d7e1c4512531769b313dffa0934eafb1b984a30
mpbrown <mpbrown@buffalo.edu>
2020-03-25 02:24:19 -0400

    Add secure SSL redirect

--- buffalomutualaid/settings/production.py
+++ buffalomutualaid/settings/production.py
@@ -16,6 +16,8 @@ DATABASES['default'] =  dj_database_url.config()
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+SECURE_SSL_REDIRECT = True
+
 # Allow all host headers
 ALLOWED_HOSTS = ['localhost','buffalomutualaid.herokuapp.com','buffalomutualaid.org']
 
