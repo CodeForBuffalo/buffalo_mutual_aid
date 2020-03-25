84f1ba6d44c42bd25441a58282fc181489904ef6
mpbrown <mpbrown@buffalo.edu>
2020-03-24 23:44:48 -0400

    Fix staticfiles_storage in base.py

--- buffalomutualaid/settings/base.py
+++ buffalomutualaid/settings/base.py
@@ -148,7 +148,7 @@ STATICFILES_DIRS = [
 # ManifestStaticFilesStorage is recommended in production, to prevent outdated
 # Javascript / CSS assets being served from cache (e.g. after a Wagtail upgrade).
 # See https://docs.djangoproject.com/en/3.0/ref/contrib/staticfiles/#manifeststaticfilesstorage
-STATICFILES_STORAGE = 'buffalomutualaid.storage.WhiteNoiseStaticFilesStorage'
+STATICFILES_STORAGE = 'buffalomutualaid.settings.storage.WhiteNoiseStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
