8397d75bd1dc66f207e3b2013f2ab727a58c50d1
mpbrown <mpbrown@buffalo.edu>
2020-03-24 23:26:39 -0400

    Remove staticfiles_storage override in production.py

--- buffalomutualaid/settings/production.py
+++ buffalomutualaid/settings/production.py
@@ -7,8 +7,6 @@ DEBUG = False
 env = os.environ.copy()
 SECRET_KEY = env['SECRET_KEY']
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
 COMPRESS_OFFLINE = True
 COMPRESS_CSS_FILTERS = [
     'compressor.filters.css_default.CssAbsoluteFilter',
